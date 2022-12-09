39564ab253eb2a6c9b416820bc2e2079dd565b80
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-12-08 20:23:55 -0500

    Rework approach

--- ShowMeTheDataStructures/Project2_task1.py
+++ ShowMeTheDataStructures/Project2_task1.py
@@ -1,6 +1,9 @@
-from asyncio.windows_events import NULL
 
-class DblNode:
+from asyncio.windows_events import NULL
+from types import NoneType
+
+
+class Node:
     def __init__(self, key, value):
         self.key = key
         self.value = value
@@ -9,92 +12,106 @@ class DblNode:
 
 
 class LRU_Cache(object):
-
+    # initialize class variables
     def __init__(self, capacity):
-        # Initialize class variables
-        self.dict = {}
-        self.capactiy = capacity
         self.head = None
         self.tail = None
+        self.cache = {}
+        self.capactiy = capacity
         self.size = 0
 
+    # remove node from current location
+    def eviction(self,node):
+        if node == self.head:
+            self.head = node.next
+            node.previous = None
+        
+        elif node == self.tail:
+            pass
 
-    def remove(self, node):
-        if self.keyPresent(node.key):
-            if node == self.head:
-                self.head = node.next
-                node.next.previous = None
-                self.size -= 1
-            elif node == self.tail:
-                self.tail = node.previous
-                node.previous.next  = None
-                self.size -= 1
-            else:
-                node.previous.next = node.next
-                node.next.previous = node.previous
-                self.size -= 1
+        else:
+            previous = node.previous
+            nxt = node.next
+            node.previous.next = nxt
+            node.next.previous = previous
 
-    def add(self, node):
+    # move most recently used node to the back of the line
+    def relocation(self,node):
+        current_tail = self.tail
+        node.previous = current_tail
+        current_tail.next = node
+        self.tail = node
+        node.next = None
+
+    # retrieve node via key if cache hit, return -1 if cache miss
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+      
+        node = self.cache[key]
+        self.eviction(node)
+        self.relocation(node)
+        return node.value
+
+    # add new node to the cache
+    def set(self, key, value):
+
+        if type(key) != NoneType or type(value) != NoneType:
+
             if self.head is None:
-                self.head = node
+                new_node = Node(key, value)
+                self.cache[key] = new_node
+                self.head = new_node
                 self.tail = self.head
                 self.size += 1
+
+            elif key in self.cache:
+                node = self.cache[key]
+                node.value = value
+                self.eviction(node)
+                self.relocation(node)   
             else:
-                node.next = self.head
-                self.head.previous = node
-                self.head = node
-                node.previous = None
+                if self.size >= self.capactiy:
+                    node = self.head
+                    del self.cache[node.key]
+                    self.eviction(node)
+                    self.size -=1
+
+                new_node = Node(key, value)
+                self.cache[key] = new_node
+                self.relocation(new_node)
                 self.size += 1
 
-    def keyPresent(self, key):
-        return key in self.dict
-
-    def get(self, key):
-        # Retrieve item from provided key. Return -1 if nonexistent. 
-        if self.keyPresent(key):
-            node = self.dict[key]
-            self.remove(node)
-            self.add(node)
-            return node.value
-        return -1
-
-    def set(self, key, value):
-        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item. 
-        if self.keyPresent(key):
-            node = self.dict[key]
-            self.remove(node)
-            self.add(node)
-        elif self.size == self.capactiy:
-            node = self.tail
-            self.remove(node)
-            self.add(DblNode(key, value))
         else:
-            self.add(DblNode(key, value))
+            return -1
 
 
 
 our_cache = LRU_Cache(5)
 
-our_cache.set(1, 1);
-our_cache.set(2, 2);
-our_cache.set(3, 3);
-our_cache.set(4, 4);
+#our_cache.set(1, 1);
+#our_cache.set(2, 2);
+#our_cache.set(3, 3);
+#our_cache.set(4, 4);
+#our_cache.set(1, 1);
 
 
-our_cache.get(1)       # returns 1
-our_cache.get(2)       # returns 2
-our_cache.get(9)      # returns -1 because 9 is not present in the cache
+#our_cache.get(1)       # returns 1
+#our_cache.get(2)       # returns 2
+#our_cache.get(9)      # returns -1 because 9 is not present in the cache
 
-our_cache.set(5, 5) 
-our_cache.set(6, 6)
+#our_cache.set(5, 5) 
+#our_cache.set(6, 6)
 
-our_cache.get(3)      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+#our_cache.get(3)      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
 
 # Add your own test cases: include at least three test cases
 # and two of them must include edge cases, such as null, empty or very large values
 
 # Test Case 1
+our_cache.set(None,None)
 
 # Test Case 2
+our_cache.set(10000*10000, 10000000*10000000)
 
 # Test Case 3
