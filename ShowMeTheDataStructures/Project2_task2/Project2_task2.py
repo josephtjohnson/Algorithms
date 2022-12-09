5337d08c71150f50f8c4b376363df0f344f34930
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-12-09 08:08:19 -0500

    Add test cases

--- ShowMeTheDataStructures/Project2_task2/Project2_task2.py
+++ ShowMeTheDataStructures/Project2_task2/Project2_task2.py
@@ -3,51 +3,69 @@ from multiprocessing.dummy import current_process
 import os
 
 
-def find_files(suffix, path):
+def find_files(suffix, path) -> str:
     """
     Find all files beneath path with file name suffix.
-
     Note that a path may contain further subdirectories
     and those subdirectories may also contain further subdirectories.
-
     There are no limit to the depth of the subdirectories can be.
-
     Args:
       suffix(str): suffix if the file name to be found
       path(str): path of the file system
-
     Returns:
        a list of paths
     """
 
-    # initialize a list for storing paths
-    path_list_local = []
+    if (isinstance(suffix, str) and isinstance(path, str)):
 
-    # does path exist
-    if os.path.exists(path) == False:
-        print('Negative Ghostrider - file doesn\'t appear to exist')
-    if os.path.isdir(path):
-        for sub_path in os.listdir(path):
-            cur_dir = os.path.join(path, sub_path)
-            if os.path.isfile(cur_dir):
-                # determine if file ends in .c
-                if os.path.splitext(sub_path)[1] == '.c':
-                    path_list_local.append(cur_dir)
-            else:
-                # if the item is a subdirectory continue down the rabbithole
-                sov = find_files(suffix, cur_dir)
-                if sov:
-                    for element in sov:
-                        path_list_local.append(element)
-    return path_list_local
+        if len(suffix) == 0:
+            print('Negative Ghostrider - must provide a file extension for searching')
+            return -1
 
-path_list = find_files('.c','C:\\Users\\jtjohns1\\Desktop\\testdir')
-a = 1
-# Add your own test cases: include at least three test cases
-# and two of them must include edge cases, such as null, empty or very large values
+        # does path exist
+        if os.path.exists(path) == False:
+            print('Negative Ghostrider - file path doesn\'t appear to exist')
+            return -1
+
+        # initialize a list for storing paths
+        path_list_local = []
+
+        if os.path.isdir(path):
+            for sub_path in os.listdir(path):
+                cur_dir = os.path.join(path, sub_path)
+                if os.path.isfile(cur_dir):
+                    # determine if file ends in the correct extension
+                    if os.path.splitext(sub_path)[1] == suffix:
+                        file = cur_dir.split('\\')[-1]
+                        path_list_local.append(file)
+                else:
+                    # if the item is a subdirectory continue down the rabbithole
+                    sov = find_files(suffix, cur_dir)
+                    if sov:
+                        for element in sov:
+                            path_list_local.append(element)
+        return path_list_local
+
+    else:
+        print('Arguments must be strings')
+        return -1
+
+print(find_files('.c','.\testdir'))
+# expected --> ['a.c', 'b.c', 'a.c', 't1.c']
+
+############ TEST CASES ############ 
 
 # Test Case 1
+print('emptyTest', find_files('','')) 
+# expected --> emptyTest, -1
 
 # Test Case 2
+print('typeTest', find_files(5,8)) 
+# expected --> typeTest, -1
 
 # Test Case 3
+print('extTest', find_files('.h','.\testdir')) 
+# expected --> extTest, ['a.h', 'b.h', 'a.h', 't1.h']
+
+x = 'breakpoint for tracing'
+
