fd4c142dd0031f7d05fc87f8c6f36c8827096ac7
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-12-12 16:27:50 -0500

    Add test cases

--- ShowMeTheDataStructures/Project2_task4/Project2_task4.py
+++ ShowMeTheDataStructures/Project2_task4/Project2_task4.py
@@ -19,20 +19,35 @@ class Group(object):
     def get_name(self):
         return self.name
 
-    def is_user_in_group(self, user):
-        """
-        Return True if user is in the group, False otherwise.
+def is_user_in_group(user, group):
+    """
+    Return True if user is in the group, False otherwise.
 
-        Args:
-          user(str): user name/id
-          group(class:Group): group to check user membership against
-        """
-        if user in self.users:
-            return True
-        for group in self.groups:
-            return group.is_user_in_group(user)
+    Args:
+        user(str): user name/id
+        group(class:Group): group to check user membership against
+    """
+    if user is None or group is None:
+        print("user and cannot be null")
         return False
 
+    if type(user) != str:
+        print("User must be a string.")
+        return False
+
+    if type(group) != Group:
+        print("Group does not exist.")
+        return False
+
+    if user in group.users:
+        return True
+    else:
+        for group in group.groups:
+            return is_user_in_group(user, group)
+    return False
+            
+  
+        
 
 
 parent = Group("parent")
@@ -46,18 +61,23 @@ sub_child_user2 = "sub_child_user"
 child.add_group(sub_child)
 parent.add_group(child)
 
-parent.is_user_in_group(sub_child_user2)
+print(is_user_in_group(sub_child_user2, sub_child))
+print(is_user_in_group(sub_child_user, child))
+print(is_user_in_group(parent, sub_child))
+
 a=1
 
 # Add your own test cases: include at least three test cases
 # and two of them must include edge cases, such as null, empty or very large values
 
 # Test Case 1
+print("Test 1", is_user_in_group("","")) # expect 
 
 # Test Case 2
+print("Test 2", is_user_in_group(12345,parent)) # expect 
 
 # Test Case 3
-
+print("Test 3", is_user_in_group(None,None)) # expect 
 
 
 
